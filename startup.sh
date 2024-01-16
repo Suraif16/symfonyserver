b77a82061c11c03262d79b9a67ee032f63b44e31
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2024-01-16 18:07:47 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 cp /home/site/wwwroot/nginx.default /etc/nginx/sites-available/default && service nginx restart
 
 # run the doctrine migration to create the database schema
-php bin/console doctrine:migrations:migrate 
+php ./bin/console doctrine:migrations:migrate 
 
 # insert data into the database
-mysql -u vigqkcybrr -pQ2MW05HIB7O3T732$ -h symfonycrudapp-server.mysql.database.azure.com symfonycrudapp-database < data.sql
+# mysql -u vigqkcybrr -pQ2MW05HIB7O3T732$ -h symfonycrudapp-server.mysql.database.azure.com symfonycrudapp-database < data.sql
